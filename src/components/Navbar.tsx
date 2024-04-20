e7264b58ec49913e2fa277fe46b6e59aa9ede851
LionStoreTeam <lionstoretechnologyteam@gmail.com>
2024-04-20 17:05:09 -0600

    Acceso-Registro Motocicletas UAEM

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import Image from "next/image";
 
 async function Navbar() {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession();
   // console.log(session);
 
   return (

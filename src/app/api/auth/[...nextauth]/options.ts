4fb4a27fdcad8f21c7eb25575b29baed3cad01da
LionStoreTeam <lionstoretechnologyteam@gmail.com>
2024-04-20 14:48:22 -0600

    Acceso-Registro Motocicletas UAEM

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,53 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "someemail@email.com",
+          autocomplete: "off",
+        },
+        password: { label: "Password", type: "password", autocomplete: "off" },
+      },
+      async authorize(
+        credentials: any,
+        req: any
+      ): Promise<{ id: string; username: string; email: string }> {
+        const userFound = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!userFound) throw new Error("Usuario no encontrado");
+
+        const passwordMatch = await bcrypt.compare(
+          credentials.password,
+          userFound.password
+        );
+
+        if (!passwordMatch) throw new Error("Contraseña incorrecta");
+
+        return {
+          id: userFound.id.toString(),
+          username: userFound.username,
+          email: userFound.email,
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/login",
+  },
+};
+export default authOptions;
+
+// 	Acceso-Registro Motocicletas UAEM
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,57 +1,5 @@
+import authOptions from "./options"; // Update the path accordingly
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
-import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-
-export const authOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "someemail@email.com",
-          autocomplete: "off",
-        },
-        password: { label: "Password", type: "password", autocomplete: "off" },
-      },
-      async authorize(
-        credentials: any,
-        req: any
-      ): Promise<{ id: string; username: string; email: string }> {
-        const userFound = await prisma.user.findUnique({
-          where: {
-            email: credentials.email,
-          },
-        });
-
-        if (!userFound) throw new Error("Usuario no encontrado");
-
-        const passwordMatch = await bcrypt.compare(
-          credentials.password,
-          userFound.password
-        );
-
-        if (!passwordMatch) throw new Error("Contraseña incorrecta");
-
-        return {
-          id: userFound.id.toString(),
-          username: userFound.username,
-          email: userFound.email,
-        };
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/auth/login",
-  },
-};
-
-const handler = NextAuth(authOptions) as never;
-
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
-
-// Acceso-Registro Motocicletas UAEM

35f06ded8b236acb12e13618cecc3ca42a1f5be1
LionStoreTeam <lionstoretechnologyteam@gmail.com>
2024-04-20 15:39:05 -0600

    Acceso-Registro Motocicletas UAEM

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,80 +1,11 @@
-// import { NextResponse } from "next/server";
-// import bcrypt from "bcrypt";
-// import { PrismaClient } from "@prisma/client";
-// import type { NextApiRequest, NextApiResponse } from "next";
-
-// const prisma = new PrismaClient();
-// export async function POST(request: { json: () => any }) {
-//   try {
-//     const data = await request.json();
-
-//     const userFound = await prisma.user.findUnique({
-//       where: {
-//         email: data.email,
-//       },
-//     });
-
-//     if (userFound) {
-//       return NextResponse.json(
-//         {
-//           message: "Email already in use",
-//         },
-//         {
-//           status: 400,
-//         }
-//       );
-//     }
-
-//     const usernameFound = await prisma.user.findUnique({
-//       where: {
-//         email: data.email,
-//       },
-//     });
-
-//     if (usernameFound) {
-//       return NextResponse.json(
-//         {
-//           message: "Username already in use",
-//         },
-//         {
-//           status: 400,
-//         }
-//       );
-//     }
-
-//     // console.log(data);
-//     const hashedPassword = await bcrypt.hash(data.password, 10);
-//     const newUser = await prisma.user.create({
-//       data: {
-//         username: data.username,
-//         email: data.email,
-//         password: hashedPassword,
-//       },
-//     });
-
-//     const { password: _, ...User } = newUser;
-
-//     return NextResponse.json(User);
-//   } catch (error: any) {
-//     return NextResponse.json(
-//       {
-//         message: error.message,
-//       },
-//       {
-//         status: 500,
-//       }
-//     );
-//   }
-// }
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export default async function POST(request: { json: () => any }) {
   try {
-    const data = req.body;
+    const data = await request.json();
 
     const userFound = await prisma.user.findUnique({
       where: {
@@ -83,10 +14,34 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
     });
 
     if (userFound) {
-      return res.status(400).json({
-        message: "Email already in use",
-      });
+      return NextResponse.json(
+        {
+          message: "Email already in use",
+        },
+        {
+          status: 400,
+        }
+      );
     }
+
+    const usernameFound = await prisma.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (usernameFound) {
+      return NextResponse.json(
+        {
+          message: "Username already in use",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    // console.log(data);
     const hashedPassword = await bcrypt.hash(data.password, 10);
     const newUser = await prisma.user.create({
       data: {
@@ -96,13 +51,17 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
       },
     });
 
-    // Omit the password field from the response
-    const { password, ...userWithoutPassword } = newUser;
+    const { password: _, ...User } = newUser;
 
-    return res.status(200).json(userWithoutPassword);
+    return NextResponse.json(User);
   } catch (error: any) {
-    return res.status(500).json({
-      message: error.message,
-    });
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
   }
 }
